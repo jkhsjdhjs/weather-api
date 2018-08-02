62fc51dd2667a1d4ed1de30b193a2fac9a45fd6f
jkhsjdhjs <leon.moeller@yahoo.de>
2018-08-02 22:00:42 +0000

    add non-tested v2 api

---
+++ api/v2/api.php
@@ -0,0 +1,20 @@
+<?php
+
+function exit_response($code, $err_msg = null, $err_subject = null, $data = null) {
+    header("Content-type: application/json");
+    http_response_code($code);
+    $error = array_filter([
+        "msg" => $err_msg,
+        "subject" => $err_subject
+    ], function ($val) { return $val !== null; });
+    exit(json_encode([
+        "error" => empty($error) ? null : $error,
+        "data" => $data
+    ]));
+}
+
+function db_conn($dsn) {
+    $dbh = new PDO($dsn);
+    $dbh->exec("SET search_path = weather_logging");
+    return $dbh;
+}
---
+++ api/v2/timespan.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once "../config.php";
+require_once "api.php";
+
+function validate_timestamp($ts) {
+    return DateTime::createFromFormat(DateTime::ATOM, $ts);
+}
+
+if($_SERVER["REQUEST_METHOD"] !== "GET")
+    exit_response(405, "invalid_request_method");
+
+$def = [
+    "start" => [
+        "filter" => FILTER_CALLBACK,
+        "options" => "validate_timestamp"
+    ],
+    "end" => [
+        "filter" => FILTER_CALLBACK,
+        "options" => "validate_timestamp"
+    ],
+    "limit" => [
+        "filter" => FILTER_VALIDATE_INT,
+        "flags"  => FILTER_REQUIRE_SCALAR,
+        "options" => [ "min_range" => 1 ]
+    ]
+];
+
+$filtered = filter_input_array(INPUT_GET, $def);
+
+foreach($filtered as $key => $val) {
+    if($val === false)
+        exit_response(400, "invalid_field_value", $key);
+    if($val === null)
+        exit_response(400, "missing_field", $key);
+}
+
+$filtered = (object) $filtered;
+
+$dbh = db_conn($pgsql_dsn);
+
+$query = $dbh->prepare(
+    "WITH f AS (
+      SELECT
+        created_at,
+        ds1820_temp as temp,
+        am2302_humidity as humidity,
+        bmp180_pressure as pressure,
+        row_number() OVER (ORDER BY created_at ASC) as n
+      FROM weather
+      WHERE created_at BETWEEN $1 AND $2
+    )
+    SELECT
+      percentile_disc(0.5) WITHIN GROUP (ORDER BY created_at) as time,
+      AVG(temp) as temp,
+      stddev_samp(temp) as temp_stddev,
+      AVG(humidity) as humidity,
+      stddev_samp(humidity) as humidity_stddev,
+      AVG(pressure) as pressure,
+      stddev_samp(pressure) as pressure_stddev
+    FROM f
+    GROUP BY ceil(n / floor((SELECT COUNT(*) FROM f) / ($3 - 1)))
+    ORDER BY time ASC"
+);
+
+if(!$query->execute([$filtered->start, $filtered->end, $filtered->limit]))
+    exit_response(500, "database_error");
+
+$rows = [];
+while(($row = $query->fetch(PDO::FETCH_OBJ)) !== false)
+    $rows[] = $row;
+
+exit_response(200, null, null, $rows);
