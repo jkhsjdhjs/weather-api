e1ce9315ec5e870ae07f7fae32460cbb64edd5b9
jkhsjdhjs <leon.moeller@yahoo.de>
2018-08-02 14:50:55 +0000

    move api key and dsn to config

--- api/v1/getLast.php
+++ api/v1/getLast.php
@@ -1,54 +1,56 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-    header("Content-Type: application/json");
-    if($_GET['key'] === "QPEqL18ovm7c4iwWtYwe7jgTJnpCjTevv0S6kGGq65MrrOTdYLr4S1R9rzSw9IKSMFgRrI3WJ5G4N4yD") {
-        $link = pg_connect("dbname=home user=home");
-        if($link) {
-            $result = pg_query($link, "SELECT * FROM weather ORDER BY id DESC LIMIT 1");
-            $row = pg_fetch_object($result);
-            $output = [
-                "error" => "null",
-                "ds1820" => [
-                    "temp" => $row->ds1820_temp,
+
+require_once "../config.php";
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+if($_GET['key'] === $api_key) {
+    $link = pg_connect($pgsql_dsn);
+    if($link) {
+        $result = pg_query($link, "SELECT * FROM weather ORDER BY id DESC LIMIT 1");
+        $row = pg_fetch_object($result);
+        $output = [
+            "error" => "null",
+            "ds1820" => [
+                "temp" => $row->ds1820_temp,
+                "unit" => "°C"
+            ],
+            "am2302" => [
+                "humidity" => [
+                    "hum" => $row->am2302_humidity,
+                    "unit" => "%"
+                ],
+                "temp" => [
+                    "temp" => $row->am2302_temp,
                     "unit" => "°C"
+                ]
+            ],
+            "bmp180" => [
+                "pressure" => [
+                    "pres" => $row->bmp180_pressure,
+                    "unit" => "hPa"
                 ],
-                "am2302" => [
-                    "humidity" => [
-                        "hum" => $row->am2302_humidity,
-                        "unit" => "%"
-                    ],
-                    "temp" => [
-                        "temp" => $row->am2302_temp,
-                        "unit" => "°C"
-                    ]
-                ],
-                "bmp180" => [
-                    "pressure" => [
-                        "pres" => $row->bmp180_pressure,
-                        "unit" => "hPa"
-                    ],
-                    "temp" => [
-                        "temp" => $row->bmp180_temp,
-                        "unit" => "°C"
-                    ]
-                ],
-                "created_at" => $row->created_at
-            ];
-        }
-        else {
-            $output = [
-                "error" => "mysqlConnError",
-                "code" => 500,
-                "msg" => "Couldn't connect to MySQL DataBase"
-            ];
-        }
+                "temp" => [
+                    "temp" => $row->bmp180_temp,
+                    "unit" => "°C"
+                ]
+            ],
+            "created_at" => $row->created_at
+        ];
     }
     else {
         $output = [
-            "error" => "badRequest",
-            "code" => 400,
-            "msg" => "Bad Request"
+            "error" => "mysqlConnError",
+            "code" => 500,
+            "msg" => "Couldn't connect to MySQL DataBase"
         ];
     }
-    echo json_encode($output);
-?>
+}
+else {
+    $output = [
+        "error" => "badRequest",
+        "code" => 400,
+        "msg" => "Bad Request"
+    ];
+}
+echo json_encode($output);
